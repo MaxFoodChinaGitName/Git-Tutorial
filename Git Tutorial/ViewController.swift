6fe673eb947e41bffc560a285846ebae8a52b2e0
FccMax <max.foodchina@gmail.com>
2020-05-22 17:27:43 +0800

    Initial Commit

---
+++ Git Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Tutorial
+//
+//  Created by fcctwit on 2020/5/22.
+//  Copyright © 2020 fcctwit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
